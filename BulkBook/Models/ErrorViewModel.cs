46536e4c3712f09498e37b4805c2c708889a0d8c
jorda <jorda@LAPTOP-OITJPGE8>
2021-10-10 19:05:52 -0400

    Add project files.

---
+++ BulkBook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BulkBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
